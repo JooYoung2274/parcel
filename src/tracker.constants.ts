eae3d984286c77c9c32f95e2c23f7cf4bf4f9f1d
joo0 <jooooo2274@gmail.com>
2023-12-22 17:38:48 +0900

    Delete src directory

--- src/tracker.constants.ts
+++
@@ -1,9 +0,0 @@
-export const PARCEL_URL = {
-    MAIN: 'https://www.cjlogistics.com/ko/tool/parcel/tracking',
-    DETAIL: 'https://www.cjlogistics.com/ko/tool/parcel/tracking-detail',
-}
-
-export const VIEW_INPUT = {
-    DATA: 'input[name=_csrf]',
-}
-
